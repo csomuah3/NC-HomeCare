52dfbb0c20d9a1641d6a5e86bf425e6c21c6245f
csomuah3 <144915674+csomuah3@users.noreply.github.com>
2024-11-26 17:14:05 +0000

    register.php

--- php/register.php
+++ php/register.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  
 $username = "root"; 
 $password = ""; 
-$dbname = "nc_homecare"; 
+$dbname = "webtech_fall2024_chelsea_somuah"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
